58c4db0af30a00bb12ac43e4112cc036f68c63b4
Eric P <ericpp@gmail.com>
2025-03-19 22:25:38 -0500

    Update tunestr text and images

--- src/element/tunestr-mission.tsx
+++ src/element/tunestr-mission.tsx
@@ -1,108 +1,75 @@
 import { Bitcoin, Radio, Users, DollarSign, Music } from "lucide-react";
 
 export default function TunestrMission() {
-  const missions = [
-    {
-      text: "Promote the growth of sovereign technology through onboarding & educating both, creators and fans to the power of Bitcoin & Nostr through V4V live performance broadcasts",
-      icon: <Bitcoin className="w-6 h-6 text-white" />,
-    },
-    {
-      text: "Create a low-risk & no-cost experience for bitcoin-curious onboarding",
-      icon: <Radio className="w-6 h-6 text-white" />,
-    },
-    {
-      text: "Provide a discovery platform for bitcoiners to connect & support like-minded artists",
-      icon: <Users className="w-6 h-6 text-white" />,
-    },
-    {
-      text: 'Introduce an additional, independent revenue stream for live performances by enabling bitcoin micropayment "tips" to performing artists worldwide',
-      icon: <DollarSign className="w-6 h-6 text-white" />,
-    },
-    {
-      text: "Keeping the art and performer as the focus of the in-person performing space and entertainment experience, while allowing the technology to enhance and expand it online",
-      icon: <Music className="w-6 h-6 text-white" />,
-    },
-  ];
-
-  const bulletPoints = [
-    "On bitcoin, there are no middle men or payment processors.",
-    "On lightning, there are minimal network fees and instant confirmation.",
-    "On Nostr, there is no censorship of artistic expression and you own all of your content and identity.",
-    "On Tunestr, all artists who represent the ethos of independent art and V4V are welcome.",
-  ];
-
   return (
     <div className="homepage">
-      <div className="flex flex-col items-center justify-center min-h-screen">
-        <img src="/tunestr-logo-t.png" alt="Logo" className="w-48 h-48" />
+      <div className="flex flex-col items-center justify-center mt-14 mb-28">
+        <img src="/tunestr-logo-t.png" alt="Tunestr Logo" className="w-48 h-48" />
         <h1 className="mt-8 text-4xl font-bold text-center">
-          INDEPENDENT MUSIC MEETS <br /> INDEPENDENT MONEY
+          <p>independent music</p>
+          <p>+</p>
+          <p>independent money</p>
         </h1>
-        <p className="mt-4 text-lg text-center text-muted-foreground">
-          Empowering Live Stream Musicians to New Heights
-        </p>
       </div>
-      <div className="mb-8 text-center">
-        <div className="flex flex-col items-center justify-center ">
-          <h1 className="text-4xl font-bold mb-2">Interactive experience</h1>
-          <p className="text-lg">
-            Connect directly with other fans through the Tunestr live chat room, see your tips on ‘top fan’ leaderboards
-            in real-time, and even control on-site lights through your bitcoin contributions.
-          </p>
-        </div>
-      </div>
-      <div className="max-w-7xl mx-auto p-4 md:p-8 mb-20">
-        <h1 className="text-4xl md:text-5xl font-bold mb-8">Mission</h1>
-        <div className="flex flex-col lg:flex-row gap-8">
-          <div className="lg:w-2/3">
-            <ul className="space-y-4">
-              {missions.map((mission, index) => (
-                <li key={index} className="flex items-start">
-                  <div className="flex-shrink-0 mr-4">
-                    <div className="bg-[#FF5500] p-3 rounded-lg">{mission.icon}</div>
-                  </div>
-                  <p className="text-sm md:text-base pt-1 max-w-xl">{mission.text}</p>
-                </li>
-              ))}
+
+      <div className="max-w-7xl mx-auto p-4 md:p-8 mb-28">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-28 mb-28 place-items-center lg:place-items-start justify-between">
+          <div className="max-w-lg">
+            <h1 className="text-3xl font-bold mb-8">
+              for independent artists
+            </h1>
+            <ul className="space-y-4 list-disc list-inside text-lg">
+              <li>generate extra revenue on your live performances</li>
+              <li>discover a new way to approach social media</li>
+              <li>engage a passionate, new fan base online</li>
+              <li>find who your biggest fans are and connect with them directly</li>
+              <li>learn about a new monetization strategy for creators</li>
             </ul>
           </div>
-          <div className="lg:w-1/2">
+          <div className="lg:w-full">
             <img
-              src="https://i.imgur.com/UmgnWV4.jpeg"
-              alt="Mission visualization"
-              className="w-full h-auto object-cover rounded-lg shadow-lg"
+              src="tunestr/antones.jpg"
+              alt="Boostagram Ball Live at Antone's in Austin, TX"
+              className="object-cover rounded-lg shadow-lg w-full"
             />
           </div>
-        </div>
-      </div>
-      <div className="max-w-6xl mx-auto p-4 md:p-8 mb-20">
-        <h1 className="text-4xl md:text-5xl font-bold mb-6 text-center lg:text-left">How It Works</h1>
-
-        <div className="flex flex-col lg:flex-row gap-8">
-          <div className="lg:w-1/2">
+          <div className="lg:w-full order-2 lg:order-1">
             <img
-              src="https://i.imgur.com/OdvbII4.jpeg"
-              alt="How It Works Visualization"
-              className="w-full h-auto object-cover rounded-lg shadow-lg"
+              src="tunestr/vinyllounge.jpg"
+              alt="The Vinyl Lounge in Nashville, TN"
+              className="object-cover rounded-lg shadow-lg w-full"
             />
           </div>
-
-          <div className="lg:w-1/2 space-y-4">
-            <p className="text-lg">
-              On Tunestr, artists can provide a free stream of their performance while fans access for free on Nostr.
-            </p>
-            <p className="text-lg">
-              If value is exchanged, the fans can contribute tips in this value-for-value system. In the V4V
-              environment, fans support artists with direct micropayments using bitcoin.
-            </p>
-
-            <ul className="list-disc pl-6 space-y-2 mt-4">
-              {bulletPoints.map((point, index) => (
-                <li key={index} className="text-lg">
-                  {point}
-                </li>
-              ))}
+          <div className="max-w-xl order-1 lg:order-2">
+            <h1 className="text-3xl font-bold mb-8">
+              for independent venues &amp; promoters
+            </h1>
+            <ul className="space-y-4 list-disc list-inside text-lg">
+              <li>differentiate yourself to touring artists</li>
+              <li>promote your venue and event to a broader audience</li>
+              <li>create a new, interactive concert experience online</li>
+              <li>stream shows with minimal effort beyond traditional streaming</li>
+              <li>help keep the economics of touring healthy</li>
             </ul>
+          </div>
+          <div className="max-w-lg order-3">
+            <h1 className="text-3xl font-bold mb-8">
+              for independent music fans
+            </h1>
+            <ul className="space-y-4 list-disc list-inside text-lg">
+              <li>prove to your favorite artists that you’re their #1 supporter</li>
+              <li>give live feedback and pay entertainers directly while they perform</li>
+              <li>connect with other fans during the show</li>
+              <li>join a new social media community where you aren’t the product</li>
+              <li>keep the independent music community thriving</li>
+            </ul>
+          </div>
+          <div className="lg:w-full order-4">
+            <img
+              src="tunestr/ainsley.jpg"
+              alt="Ainsley Costello"
+              className="object-cover rounded-lg shadow-lg w-full"
+            />
           </div>
         </div>
       </div>
